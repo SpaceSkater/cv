20ee175de41f92c430edf43d69e3701c2888e392
Sake <santalazarus@gmail.com>
2024-05-30 21:42:51 +0800

    dev branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import { RESUME_DATA } from "@/data/resume-data";
 import { ProjectCard } from "@/components/project-card";
 
 // dev
+// dev branch
 
 export const metadata: Metadata = {
   title: `${RESUME_DATA.name} | ${RESUME_DATA.about}`,
